5f9562663632022bfbc8573f7695384299b33d47
Zainab <zainabzaidi2005@gmail.com>
2026-09-25 18:52:33 -0400

    Complete week 1 plotting question

--- week1_plot.py
+++ week1_plot.py
@@ -2,12 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # TODO: load the dataset as pandas dataframe
+df_teeth = pd.read_csv("mammal_teeth.csv")
 
 plt.figure(figsize=(5, 10)) # set figure size
 plt.scatter(x=df_teeth['Top incisors'],
 y=df_teeth['MAMMAL']) # set figure x, y axis
 plt.gca().xaxis.set_visible(False)
 
-# TODO: change the title name to include your name
-plt.title("plot for mammal_teeth dataset")
+plt.title(" Zainab's plot for mammal_teeth dataset")
 plt.savefig("mammal_teeth_scatterplot.png", dpi=150) # save the figure
